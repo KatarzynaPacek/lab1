769adca2c072c954c8803abbefa27c3a2e69670a
Katarzyna Pacek <205832@student.pwr.edu.pl>
2016-03-31 22:31:24 +0200

    Dodanie rozwiazania zadania lab1

--- main.cpp
+++ main.cpp
@@ -1,24 +1,63 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <cstdlib>
 #include <time.h>
+#include <iostream>
+
+using namespace std;
+
 
 char **charSquare(int n);
 void drawCharSquare(char **square, int n);
 
 int main(int argc, char **argv)
 {
-	int n;
-	drawCharSquare(charSquare(n), n);	
+	if(argc < 3) 
+		{ 
+			cout << "wrong number of arguments";
+			return 0;
+		}
+
+int b = atoi(argv[2]);
+srand(b);
+
+
+int  n = atoi(argv[1]) ;
+drawCharSquare(charSquare(n), n);
+
 }
 
 char **charSquare(int n)
 {
-	char **square;
+
+	static char **square;
+
+	square = new char*[n];
+
+	for(int i = 0; i<n ; ++i)
+	{
+		square[i] = new char[n];
+	}
+
+	for(int i = 0; i < n*n ; ++i)
+	{
+		square[i/n][i%n] = ((rand() % 26) + 97);
+		
+	}
+
+
 	return square;
 }
 
 void drawCharSquare(char **square, int n)
 {
+
+	for(int i = 0; i < n*n ; ++i)
+	{
+		i%n==n-1?cout<<square[i/n][i%n]<<endl:cout<<square[i/n][i%n]<<" ";
+	}
+	
+
+}
 
 
 //deded
